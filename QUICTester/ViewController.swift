c70c8124b0f1a1c31a190a5e3dae97f3772645a6
Quentin De Coninck <quentindeconinck@wifi-secure6-115.sri.ucl.ac.be>
2017-09-06 10:27:14 +0200

    Initial Commit

---
+++ QUICTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QUICTester
+//
+//  Created by Quentin De Coninck on 9/6/17.
+//  Copyright © 2017 Universite Catholique de Louvain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
