272e4d285f2b4b51b18f63f49c0a79e1b493b3d2
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2017-09-08 15:18:37 +0200

    First user-friendly interface to launch QUIC tests

--- QUICTester/ViewController.swift
+++ QUICTester/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 import Client
 
 class ViewController: UIViewController {
-
+    // MARK: Properties
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Starting")
-        let res = ClientRun(true, true, "", "https://ns387496.ip-176-31-249.eu:6121/random")
-        print(res)
-        print("Alibababababa End")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Actions
+    @IBAction func startTestButtonPressed(_ sender: UIButton) {
+        // Avoid launching multiples tests in parralel
+        sender.isEnabled = false
+        
+        print("Starting")
+        let res = ClientRun(true, true, "", "https://ns387496.ip-176-31-249.eu:6121/random")
+        print("End with time \(String(describing: res))")
+        
+        // Show the result to the user
+        resultLabel.text = res
+        
+        // Then reactivate the button
+        sender.isEnabled = true
+    }
+    
 }
 
