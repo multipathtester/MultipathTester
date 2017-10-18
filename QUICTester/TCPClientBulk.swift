28d421392bbf49852f0f31382ed7099448eafe4a
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2017-10-18 15:56:17 +0200

    print TCP info in bulk traffic

--- QUICTester/TCPClientBulk.swift
+++ QUICTester/TCPClientBulk.swift
@@ -55,7 +55,38 @@ class TCPClientBulk {
             task.resume()
         }
         
-        group.wait()
+        usleep(10000)
+        
+        // This is quite ugly, but Apple does not provide an easy way to collect this information...
+        var saddr = sockaddr()
+        var slen: socklen_t = socklen_t(MemoryLayout<sockaddr>.size)
+        var tlen: socklen_t = socklen_t(MemoryLayout<tcp_connection_info>.size)
+        var tcpi = tcp_connection_info()
+        var theFd: Int32 = 0
+        // FIXME consider changing this later to adapt to changing fd
+        for fd in 0...100 {
+            let err = getpeername(Int32(fd), &saddr, &slen)
+            if err == -1 {
+                //print("Error occurred with fd \(fd): errno is \(errno)")
+            } else {
+                // FIXME check if the addr is the right one
+                print(err, fd, saddr.sa_data, slen, saddr, saddr.sa_len)
+                theFd = Int32(fd)
+            }
+        }
+        
+        var res = group.wait(timeout: DispatchTime.now() + 0.01)
+        while (res == .timedOut) {
+            let err2 = getsockopt(theFd, IPPROTO_TCP, TCP_CONNECTION_INFO, &tcpi, &tlen)
+            if err2 != 0 {
+                print(err2, errno, ENOPROTOOPT)
+            } else {
+                print(tcpi)
+            }
+            res = group.wait(timeout: DispatchTime.now() + 0.01)
+        }
+        
+        // group.wait()
         session.finishTasksAndInvalidate()
         
         return ret
