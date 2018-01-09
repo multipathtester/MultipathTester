8bbe48c9e815bbde6c38c662e63275e3e3505f5e
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2018-01-09 14:41:16 +0100

    Working on the UI with the mobile tests screen

---
+++ QUICTester/ChartsHelpers/Charts/LineChartHelper.swift
@@ -0,0 +1,81 @@
+//
+//  LineChartInitializer.swift
+//  QUICTester
+//
+//  Created by Quentin De Coninck on 1/9/18.
+//  Copyright © 2018 Universite Catholique de Louvain. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class LineChartHelper {
+    static func initialize(chartView: LineChartView, delegate: ChartViewDelegate?) {
+        //chartView.noDataText = "Run tests to see SNR evolution"
+        
+        chartView.delegate = delegate
+        
+        chartView.chartDescription?.enabled = false
+        
+        chartView.dragEnabled = true
+        chartView.setScaleEnabled(true)
+        chartView.pinchZoomEnabled = true
+        chartView.highlightPerDragEnabled = true
+        
+        chartView.backgroundColor = .white
+        
+        //chartView.legend.enabled = false
+        
+        let xAxis = chartView.xAxis
+        xAxis.labelPosition = .topInside
+        xAxis.labelFont = .systemFont(ofSize: 10, weight: .light)
+        xAxis.labelTextColor = UIColor(red: 255/255, green: 192/255, blue: 56/255, alpha: 1)
+        xAxis.drawAxisLineEnabled = false
+        xAxis.drawGridLinesEnabled = true
+        xAxis.centerAxisLabelsEnabled = true
+        xAxis.granularity = 1
+        xAxis.valueFormatter = DateValueFormatter()
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.labelPosition = .insideChart
+        leftAxis.labelFont = .systemFont(ofSize: 12, weight: .light)
+        leftAxis.drawGridLinesEnabled = true
+        leftAxis.granularityEnabled = true
+        leftAxis.axisMinimum = 0
+        //leftAxis.axisMaximum = 170
+        leftAxis.yOffset = -9
+        leftAxis.labelTextColor = UIColor(red: 255/255, green: 192/255, blue: 56/255, alpha: 1)
+        
+        let marker = BalloonMarker(color: UIColor(white: 180/255, alpha: 1),
+                                   font: .systemFont(ofSize: 12),
+                                   textColor: .white,
+                                   insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8))
+        marker.chartView = chartView
+        marker.minimumSize = CGSize(width: 80, height: 40)
+        chartView.marker = marker
+        
+        
+        chartView.rightAxis.enabled = false
+        
+        chartView.legend.form = .line
+    }
+    
+    static func setData(to chartView: LineChartView, with values: [ChartDataEntry], label: String) {
+        let set1 = LineChartDataSet(values: values, label: label)
+        set1.axisDependency = .left
+        set1.setColor(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
+        set1.lineWidth = 1.5
+        set1.drawCirclesEnabled = false
+        set1.drawValuesEnabled = true
+        set1.fillAlpha = 0.26
+        set1.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
+        set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        set1.drawCircleHoleEnabled = false
+        
+        let data = LineChartData(dataSet: set1)
+        data.setValueTextColor(.white)
+        data.setValueFont(.systemFont(ofSize: 9, weight: .light))
+        
+        chartView.data = data
+    }
+}
---
+++ QUICTester/MobileTesterViewController.swift
@@ -0,0 +1,69 @@
+//
+//  MobileTesterViewController.swift
+//  QUICTester
+//
+//  Created by Quentin De Coninck on 1/8/18.
+//  Copyright © 2018 Universite Catholique de Louvain. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class MobileTesterViewController: UIViewController, ChartViewDelegate {
+    @IBOutlet weak var distanceChartView: LineChartView!
+    @IBOutlet weak var snrDelayChartView: LineChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        LineChartHelper.initialize(chartView: distanceChartView, delegate: self)
+        MultiLineChartHelper.initialize(chartView: snrDelayChartView, delegate: self)
+        
+        self.updateChartData()
+        distanceChartView.animate(xAxisDuration: 0.01 * Double(distanceChartView.data!.entryCount))
+        snrDelayChartView.animate(xAxisDuration: 0.01 * Double(snrDelayChartView.data!.entryCount / 2))
+    }
+    
+    func updateChartData() {
+        self.setDataCount(Int(3), range: 30)
+    }
+    
+    func setDataCount(_ count: Int, range: UInt32) {
+        let now = Date().timeIntervalSince1970
+        let minuteSeconds: TimeInterval = 60
+        
+        let from = now - (Double(count) / 2) * minuteSeconds
+        let to = now + (Double(count) / 2) * minuteSeconds
+        
+        let values1 = stride(from: from, to: to, by: 1).map { (x) -> ChartDataEntry in
+            let val = ((from - x) * 30.0 / (to - from)) - 60
+            return ChartDataEntry(x: x, y: val)
+        }
+        
+        let values2 = stride(from: from, to: to, by: 1).map { (x) -> ChartDataEntry in
+            let y = arc4random_uniform(range) + 50
+            return ChartDataEntry(x: x, y: Double(y))
+        }
+        
+        LineChartHelper.setData(to: distanceChartView, with: values2, label: "Distance")
+        MultiLineChartHelper.setData(to: snrDelayChartView, with1: values1, label1: "WiFi SNR (dB)", with2: values2, label2: "Delay (ms)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
